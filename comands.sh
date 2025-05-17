9a199c86364f5984298a2f18357fb7fdeb0e47f4
arthur <art5mussi@outlook.com>
2025-05-17 18:09:40 -0300

    feat: organização de ambiente

---
+++ comands.sh
@@ -0,0 +1,14 @@
+# Lista de comandos
+
+## Criar projeto
+npx react-native init note-app
+cd ./note-app
+
+## Instalar dependências
+npx expo install expo-sqlite
+
+## Iniciar projeto
+npx expo start
+
+## Buildar apk
+npx expo run:android
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Home } from './src/screen/home';
+import { Home } from './screen/home';
 export default function App() {
   return (
     <Home />
+    
   );
 }
 
